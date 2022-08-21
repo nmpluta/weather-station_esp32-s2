439ab6b36c5c600a9aa2c59f631830acdb84f7b2
Natalia Pluta <pluta.natalia.m@gmail.com>
2022-08-21 19:51:54 +0200

    wifi: add working in STA mode

---
+++ project/weather-station/include/task_wifi.h
@@ -0,0 +1,6 @@
+#ifndef TASK_WIFI_H
+#define TASK_WIFI_H
+
+void task_wifi(void *pvParameter);
+
+#endif // TASK_WIFI_H
--- project/weather-station/src/main.c
+++ project/weather-station/src/main.c
@@ -7,10 +7,12 @@
 #include "task_air_sensor.h"
 #include "task_bme280.h"
 #include "task_oled.h"
+#include "task_wifi.h"
 
 void app_main() 
 {
-    xTaskCreate(&task_air_sensor, "task_air_sensor", 2048, NULL, 5, NULL);
-    xTaskCreate(&task_bme280, "task_bme280", 2048, NULL, 5, NULL);
-    xTaskCreate(&task_oled, "task_oled", 2048, NULL, 5, NULL);
+    // xTaskCreate(&task_air_sensor, "task_air_sensor", 2048, NULL, 5, NULL);
+    // xTaskCreate(&task_bme280, "task_bme280", 2048, NULL, 5, NULL);
+    // xTaskCreate(&task_oled, "task_oled", 2048, NULL, 5, NULL);
+    xTaskCreate(&task_wifi, "task_wifi", 4096, NULL, 5, NULL);
 }
